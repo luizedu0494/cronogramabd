242432c7653fa6724db3c7ab645b5fb775ed1cdc
Luiz <dgjheuh@gmail.com>
2026-09-13 02:12:53 -0300

    fix(groq): exportacao module.exports compativel com Vercel Node handler para solucionar status 405

--- api/groq.js
+++ api/groq.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -59,5 +59,7 @@ export default async function handler(req, res) {
     console.error('Erro na Vercel Function groq Proxy:', error);
     return res.status(500).json({ error: 'Erro interno ao processar requisição da IA: ' + (error.message || String(error)) });
   }
-}
+};
+
+module.exports.default = module.exports;
 
